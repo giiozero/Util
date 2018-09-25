e7d62db6a8b2c32db2083753983432a19d94da74
Giovanni Vassallo <giovassallo@hotmail.com>
2018-09-25 16:22:48 -0300

    Update consultaCNPJ.php

--- ConsultaCNPJ/consultaCNPJ.php
+++ ConsultaCNPJ/consultaCNPJ.php
@@ -32,5 +32,62 @@ function curlExec($url, $post = NULL, array $header = array()){
     return $data;
 }
 $teste = curlExec("http://receitaws.com.br/v1/cnpj/".$cnpj);
+echo '<strong>Array obtido (bruto)</strong><br/>';
 echo $teste;
+
+$obj = json_decode($teste);
+echo '<br/><br/><strong>Array obtido (Pré-Tratado)</strong></br>';
+//busca o nome
+$nome = $obj->nome;
+//busca o tipo = Matriz/filial
+$tipo = $obj->tipo;
+echo '<B>CNPJ</B> - '.$obj->cnpj.'</br>';
+echo 'R. Social: '.$nome.' ('.$tipo.')</br>';
+echo  'Fantasia: '.$obj->fantasia.'<br/>';
+
+echo 'Situação: '.$obj->situacao.' (Desde '.$obj->abertura.')<br/>';
+echo '<br/>'.$obj->logradouro.', '.$obj->numero.' - '.$obj->bairro.' - '.$obj->municipio.'/ '.$obj->uf.' - '.$obj->cep.'<br/>';
+
+
+//busca a atividade principal
+$atividade_principal = $obj->atividade_principal;
+echo '<br/><b>Atividade(s) Econômica(s):</b>';
+foreach ($atividade_principal as $a) {
+   echo "<br/> - $a->text ";//$a->code 
+}
+
+//busca as atividades secundárias
+$atividades_secundarias = $obj->atividades_secundarias;
+echo "<br/><b>Atividades secundárias:</b><br/> ";
+foreach ($atividades_secundarias as $a){
+   echo "$a->text ($a->code)<br/>";
+}
+//busca Sócios
+$socios = $obj->qsa;
+echo "</br>Sócios: ";
+foreach ($socios as $a){
+   echo "$a->nome ($a->qual) | ";
+}
+echo 'R$ '.$obj->capital_social.'</br>';
+
+
+echo '<br/> Nat Jurídica - '.$obj->natureza_juridica
+ .'<br/>Status - '.$obj->status
+ .'<br/>'.$obj->complemento
+ .'<br/>Email - '.$obj->email
+
+
+
+//------------------Outros parâmetros que podem ser buscados ----------------------
+//busca a data da situaçao
+//$data_situacao = $obj->data_situacao;
+//echo "<br/>Data de situação: $data_situacao </br>";
+
+/*
+ .'<br/>EFR - '.$obj->efr
+ .'<br/>MTV - '.$obj->motivo_situacao
+ .'<br/>ESP - '.$obj->situacao_especial
+ .'<br/>DT ESP - '.$obj->data_situacao_especial
+ .'<br/>U ATT - '.$obj->ultima_atualizacao
+*/
 ?>
